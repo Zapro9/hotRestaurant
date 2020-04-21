1a0e0145fa64ebd08d5c1a201c7d385b85353292
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-04-21 14:22:41 -0600

    added listener and path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -6,3 +7,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
