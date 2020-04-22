6698c29100f649face70308506a2969cff022b6f
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-04-22 10:59:24 -0600

    fixed css problem

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 var availableTables = 10;
